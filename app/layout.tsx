6728cc4eada8f1ea36c61e6a987255fb9cd6ea40
nkurunziza-saddy <saadnkurunziza@email.com>
2025-09-07 10:00:37 +0200

    better metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono, Geist } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { siteConfig } from "@/lib/config";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -9,8 +10,47 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "sanuru",
-  description: "Saddy's portfolio",
+  title: siteConfig.name,
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+  description: siteConfig.description,
+  keywords: [
+    "saddy",
+    "nkurunziza",
+    "job",
+    "react",
+    "engineer",
+    "fullstack",
+    "rust",
+    "go",
+    "next.js",
+    "js",
+    "rwanda",
+    "africa",
+  ],
+  authors: [
+    {
+      name: "Nkurunziza Saddy",
+      url: "https://github.com/nkurunziza-saddy",
+    },
+  ],
+  creator: "saddy",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: process.env.NEXT_PUBLIC_APP_URL!,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    creator: "@saddy",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 const geistSans = Geist({
---
+++ lib/config.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "Nkurunziza Saddy",
+  url: "https://saddynkurunziza.vercel.app/",
+  description: "Owns a computer and actually does cool stuff with it.",
+  links: {
+    twitter: "https://twitter.com/nk-saddy",
+    github: "https://github.com/nkurunziza-saddy",
+  },
+};
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
