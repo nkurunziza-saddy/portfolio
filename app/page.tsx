0a62930c96c0c15ee7911dc724d888b2536c75b7
nkurunziza-saddy <saadnkurunziza@gmail.com>
2026-05-16 15:43:40 +0200

    add python language

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
         <h1 className="text-base font-medium text-foreground tracking-tight">Saddy Nkurunziza</h1>
         <p className="text-sm text-muted-foreground leading-relaxed max-w-[500px]">
           Software engineer focused on building clean, functional, and well-designed digital experiences. Working
-          primarily with Typescript, Go, and Rust.
+          primarily with Typescript, Go, Rust and Python.
         </p>
       </header>
 
